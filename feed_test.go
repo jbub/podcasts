c5ac4a72a15210e11c561636f1335a57e366a826
jbub <contact@jbub.eu>
2014-12-20 08:40:25 +0100

    updates

--- feed_test.go
+++ feed_test.go
@@ -8,6 +8,22 @@ import (
 
 type FeedTestSuite struct {
 	suite.Suite
+	feed *Feed
+}
+
+func (s *FeedTestSuite) SetupTest() {
+	s.feed = &Feed{
+		Xmlns:   rssXmlns,
+		Version: rssVersion,
+		Channel: &Channel{
+			Title:       p.Title,
+			Description: p.Description,
+			Link:        p.Link,
+			Copyright:   p.Copyright,
+			Language:    p.Language,
+			Items:       p.items,
+		},
+	}
 }
 
 func (s *FeedTestSuite) TestFeed() {
