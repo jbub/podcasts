4120feb714fc2b628cf39230355fba8df60f9ec6
jbub <contact@jbub.eu>
2015-02-24 17:13:50 +0100

    update tests

--- podcast_test.go
+++ podcast_test.go
@@ -2,17 +2,11 @@ package podcasts
 
 import (
 	"bytes"
-	"encoding/xml"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
 )
-
-type validFeed struct {
-	Channel struct {
-		Title string `xml:"title"`
-	} `xml:"channel"`
-}
 
 type PodcastsTestSuite struct {
 	suite.Suite
@@ -20,33 +14,70 @@ type PodcastsTestSuite struct {
 }
 
 func (s *PodcastsTestSuite) SetupTest() {
-	title := "test TITLE"
-	desc := "description"
-	lang := "lang"
-	link := "link"
-	copyright := "copyright"
-
 	s.podcast = &Podcast{
-		Title:       title,
-		Description: desc,
-		Language:    lang,
-		Link:        link,
-		Copyright:   copyright,
+		Title:       "my podcast title",
+		Description: "my podcast description",
+		Language:    "my podcast lang",
+		Link:        "my podcast link",
+		Copyright:   "my podcast copyright",
 	}
 }
 
-func (s *PodcastsTestSuite) TestPodcastFeedXml() {
-	feed, err := s.podcast.Feed()
-	s.Nil(err)
-
-	data, err := feed.XML()
-	s.Nil(err)
-
-	vf := validFeed{}
-	err = xml.Unmarshal([]byte(data), &vf)
+func (s *PodcastsTestSuite) TestContainsXmlHeader() {
+	data, err := getPodcastXML(s.podcast)
 
 	s.Nil(err)
-	s.Equal(s.podcast.Title, vf.Channel.Title)
+	s.Contains(data, `<?xml version="1.0" encoding="UTF-8"?>`)
+}
+
+func (s *PodcastsTestSuite) TestContainsRssElement() {
+	data, err := getPodcastXML(s.podcast)
+
+	s.Nil(err)
+	s.Contains(data, `<rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">`)
+}
+
+func (s *PodcastsTestSuite) TestContainsChannelElement() {
+	data, err := getPodcastXML(s.podcast)
+
+	s.Nil(err)
+	s.Contains(data, `<channel>`)
+	s.Contains(data, `</channel>`)
+}
+
+func (s *PodcastsTestSuite) TestContainsTitleElement() {
+	data, err := getPodcastXML(s.podcast)
+
+	s.Nil(err)
+	s.Contains(data, fmt.Sprintf("<title>%s</title>", s.podcast.Title))
+}
+
+func (s *PodcastsTestSuite) TestContainsDescriptionElement() {
+	data, err := getPodcastXML(s.podcast)
+
+	s.Nil(err)
+	s.Contains(data, fmt.Sprintf("<description>%s</description>", s.podcast.Description))
+}
+
+func (s *PodcastsTestSuite) TestContainsLanguageElement() {
+	data, err := getPodcastXML(s.podcast)
+
+	s.Nil(err)
+	s.Contains(data, fmt.Sprintf("<language>%s</language>", s.podcast.Language))
+}
+
+func (s *PodcastsTestSuite) TestContainsLinkElement() {
+	data, err := getPodcastXML(s.podcast)
+
+	s.Nil(err)
+	s.Contains(data, fmt.Sprintf("<link>%s</link>", s.podcast.Link))
+}
+
+func (s *PodcastsTestSuite) TestContainsCopyrightElement() {
+	data, err := getPodcastXML(s.podcast)
+
+	s.Nil(err)
+	s.Contains(data, fmt.Sprintf("<copyright>%s</copyright>", s.podcast.Copyright))
 }
 
 func (s *PodcastsTestSuite) TestPodcastFeedWrite() {
@@ -60,4 +91,12 @@ func (s *PodcastsTestSuite) TestPodcastFeedWrite() {
 
 func TestPodcastsTestSuite(t *testing.T) {
 	suite.Run(t, new(PodcastsTestSuite))
+}
+
+func getPodcastXML(p *Podcast) (string, error) {
+	feed, err := p.Feed()
+	if err != nil {
+		return "", err
+	}
+	return feed.XML()
 }
