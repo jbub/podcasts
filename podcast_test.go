1b2f7d158ed769a078b4574131c60f6147d57d72
jbub <contact@jbub.eu>
2015-02-24 17:25:31 +0100

    update test

--- podcast_test.go
+++ podcast_test.go
@@ -80,6 +80,79 @@ func (s *PodcastsTestSuite) TestContainsCopyrightElement() {
 	s.Contains(data, fmt.Sprintf("<copyright>%s</copyright>", s.podcast.Copyright))
 }
 
+func (s *PodcastsTestSuite) TestContainsBlockElement() {
+	data, err := getPodcastXML(s.podcast, Block)
+
+	s.Nil(err)
+	s.Contains(data, fmt.Sprintf("<itunes:block>%s</itunes:block>", ValueYes))
+}
+
+func (s *PodcastsTestSuite) TestContainsExplicitElement() {
+	data, err := getPodcastXML(s.podcast, Explicit)
+
+	s.Nil(err)
+	s.Contains(data, fmt.Sprintf("<itunes:explicit>%s</itunes:explicit>", ValueYes))
+}
+
+func (s *PodcastsTestSuite) TestContainsCompleteElement() {
+	data, err := getPodcastXML(s.podcast, Complete)
+
+	s.Nil(err)
+	s.Contains(data, fmt.Sprintf("<itunes:complete>%s</itunes:complete>", ValueYes))
+}
+
+func (s *PodcastsTestSuite) TestContainsAuthorElement() {
+	author := "Test Author"
+	data, err := getPodcastXML(s.podcast, Author(author))
+
+	s.Nil(err)
+	s.Contains(data, fmt.Sprintf("<itunes:author>%s</itunes:author>", author))
+}
+
+func (s *PodcastsTestSuite) TestContainsNewFeedURLElement() {
+	url := "http://localhost/my-test-url"
+	data, err := getPodcastXML(s.podcast, NewFeedURL(url))
+
+	s.Nil(err)
+	s.Contains(data, fmt.Sprintf("<itunes:new-feed-url>%s</itunes:new-feed-url>", url))
+}
+
+func (s *PodcastsTestSuite) TestContainsSubtitleElement() {
+	subtitle := "Test Subtitle"
+	data, err := getPodcastXML(s.podcast, Subtitle(subtitle))
+
+	s.Nil(err)
+	s.Contains(data, fmt.Sprintf("<itunes:subtitle>%s</itunes:subtitle>", subtitle))
+}
+
+func (s *PodcastsTestSuite) TestContainsSummaryElement() {
+	summary := "Test Subtitle"
+	data, err := getPodcastXML(s.podcast, Summary(summary))
+
+	s.Nil(err)
+	s.Contains(data, fmt.Sprintf("<itunes:summary>%s</itunes:summary>", summary))
+}
+
+func (s *PodcastsTestSuite) TestContainsOwnerElement() {
+	name := "Test Name"
+	email := "test@name.com"
+	data, err := getPodcastXML(s.podcast, Owner(name, email))
+
+	s.Nil(err)
+	s.Contains(data, "<itunes:owner>")
+	s.Contains(data, fmt.Sprintf("<itunes:name>%s</itunes:name>", name))
+	s.Contains(data, fmt.Sprintf("<itunes:email>%s</itunes:email>", email))
+	s.Contains(data, "</itunes:owner>")
+}
+
+func (s *PodcastsTestSuite) TestContainsImageElement() {
+	image := "http://localhost/myimage.jpg"
+	data, err := getPodcastXML(s.podcast, Image(image))
+
+	s.Nil(err)
+	s.Contains(data, fmt.Sprintf(`<itunes:image href="%s"></itunes:image>`, image))
+}
+
 func (s *PodcastsTestSuite) TestPodcastFeedWrite() {
 	feed, err := s.podcast.Feed()
 	s.Nil(err)
@@ -93,8 +166,8 @@ func TestPodcastsTestSuite(t *testing.T) {
 	suite.Run(t, new(PodcastsTestSuite))
 }
 
-func getPodcastXML(p *Podcast) (string, error) {
-	feed, err := p.Feed()
+func getPodcastXML(p *Podcast, options ...func(f *Feed) error) (string, error) {
+	feed, err := p.Feed(options...)
 	if err != nil {
 		return "", err
 	}
